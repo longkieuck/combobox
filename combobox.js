94e6347c647675000ea576f56778ac5e58d73487
longkieuck <longkieuck@gmail.com>
2021-04-26 01:21:40 +0700

    combobox

--- combobox.js
+++ combobox.js
@@ -1,142 +1,162 @@
-$(document).ready(function(){
-    let a= [{text:"Nam",value:0},{text:"Nữ",value:1},{text:"Khác",value:2},]
-    let aFilter = [{text:"Nam",value:0},{text:"Nữ",value:1},{text:"Khác",value:2},]
-    let selected = 0
-    let beforeSelected = 0
-    let show = true
-    a.forEach(i =>{
-        if(i.value === selected){
-            $('#table-option').append(`<tr class="tr-selected" style="color: green;" data-optionid="${i.value}">
-                                <td>${i.text}</td>
-                            </tr>`)
-        }
-        else
-        $('#table-option').append(`<tr style="color: green;" data-optionid="${i.value}">
-                                <td>${i.text}</td>
-                            </tr>`)
-    })
-    $("tbody tr").click(function(e){
-        selected=$(this).attr("data-optionid")
-        $(this).siblings().removeClass('tr-selected');
-        $(this).addClass('tr-selected')
-        // alert($(this).attr("data-optionid"))
-    })
-    $("#txtGender").on('input',function(e){
-        show=true
+$(document).ready(function() {
+    let options = [{ text: "Nam", value: 0 }, { text: "Nữ", value: 1 }, { text: "Khác", value: 2 }, ]
+    let filterOptions = []
+    let selected = { text: "", value: -1 }
+    let curPosition = 0 //Vị trí con trỏ hiện tại
+        //Ẩn
+    let show = false
+    $("#table-option").hide()
+
+    $("#txtGender").on('input', function(e) {
+        show = true
+
+        filterOptions = options.filter(i => i.text.includes(e.target.value))
+
+        $("tbody").empty()
+        filterOptions.forEach(i => {
+
+            if (i.value == selected.value) {
+                $('#table-option').append(`<tr class="tr-selected" value="${i.value}">
+                            <td>${i.text}</td>
+                        </tr>`)
+            } else
+                $('#table-option').append(`<tr value="${i.value}">
+                            <td>${i.text}</td>
+                        </tr>`)
+        })
+
+
+
+
         $("#table-option").show()
-        if(e.target.value != ""){
-            $("#table-option > tbody > tr").each(function(index, tr) { 
-                if(!a[index].text.includes(e.target.value))
-                $(tr).hide()
-                else $(tr).show()
-             });
-        }else{
-            $("#table-option > tbody > tr").each(function(index, tr) { 
-                $(tr).show()
-             });
-        }
-        // console.log(e.target.value)
-    })
-    $("#btn-show").click(function(e){
-        if(show === true){
-            $("#table-option").hide()
-            show = false
-        }else{
-            $("#table-option").show()
-            show = true
-        }
-    })
-    // $(document).click(function(e){
-    //     if(show === true){
-    //         $("#table-option").addClass('hidden-option')
-    //         show = false
-    //     }
-    // })
-    window.addEventListener('click', function(event){
-        var box = document.getElementById('table-option');
-        var bt = document.getElementById('btn-show');
-        if (event.target != box && event.target.parentNode != box && event.target != bt){
-            if(show === true){
-            $("#table-option").hide()
-            show = false
-        }
-        }
-    });
-    $(function(){
-        $(document).keydown(function(e){
-            if(show === true){
-                //down
-                if(e.which === 40){
-                    if(beforeSelected ==(a.length-1)){
-                        beforeSelected = 0
-                    }else beforeSelected++
-                    setTrBeforeSelectedStyle(beforeSelected,selected)
-                    $("#txtGender").val(a[beforeSelected].text)
-                }
-                //up
-                if(e.which === 38){
-                    if(beforeSelected == 0){
-                        beforeSelected = a.length-1
-                    }else beforeSelected--
-                    setTrBeforeSelectedStyle(beforeSelected,selected)
-                    $("#txtGender").val(a[beforeSelected].text)
-                }
-                if(e.which === 13){
-                    selected = beforeSelected
-                    setTrSelectedStyle(selected)
-                    $("#table-option").hide()
-                    $("#txtGender").val(a[selected].text)
-                }
-            }
-        });
-    });
-    $(function(){
-        $("#table-option > tbody > tr").mouseover(function(){
-            // console.log($(this).attr("data-optionid"))
-            $("#txtGender").val(a[$(this).attr("data-optionid")].text)
-            $(this).siblings().removeClass('tr-before-selected');
+        $("tbody tr").click(function(e) {
+            selected = options[$(this).attr("value")]
+            $("#txtGender").val(selected.text)
+            $(this).siblings().removeClass('tr-selected');
+            $(this).addClass('tr-selected')
         })
     })
 
+    $("#btn-show").click(async function(e) {
+        if (show == true) {
+            $("#table-option").hide()
+            show = false
+        } else {
+            await $("tbody").empty()
+            await options.forEach(i => {
+
+                if (i.value == selected.value) {
+                    $('#table-option').append(`<tr class="tr-selected" value="${i.value}">
+                                    <td>${i.text}</td>
+                                </tr>`)
+                } else
+                    $('#table-option').append(`<tr value="${i.value}">
+                                    <td>${i.text}</td>
+                                </tr>`)
+            })
+            await $("#table-option").show()
+            $("tbody tr").click(function(e) {
+                selected = options[$(this).attr("value")]
+                $("#txtGender").val(selected.text)
+                $(this).siblings().removeClass('tr-selected');
+                $(this).addClass('tr-selected')
+            })
+
+            $(function() {
+                $("#table-option > tbody > tr").mouseover(function() {
+                    // console.log($(this).attr("value"))
+                    $("#txtGender").val(options[$(this).attr("value")].text)
+                    $(this).siblings().removeClass('tr-before-selected');
+                })
+            })
+
+            show = true
+        }
+    })
+
+    window.addEventListener('click', function(event) {
+        var box = document.getElementById('table-option');
+        var bt = document.getElementById('btn-show');
+        if (event.target != box && event.target.parentNode != box && event.target != bt) {
+            if (show == true) {
+                $("#table-option").hide()
+                show = false
+            }
+        }
+    });
+    $(function() {
+        $(document).keydown(function(e) {
+            if (show == true) {
+                //down
+                if (e.which === 40) {
+                    if (curPosition == (filterOptions.length - 1)) {
+                        curPosition = 0
+                    } else curPosition++
+                        setTrBeforeSelectedStyle(curPosition)
+                    $("#txtGender").val(filterOptions[curPosition].text)
+                }
+                //up
+                if (e.which === 38) {
+                    if (curPosition == 0) {
+                        curPosition = filterOptions.length - 1
+                    } else curPosition--
+                        setTrBeforeSelectedStyle(curPosition)
+                    $("#txtGender").val(filterOptions[curPosition].text)
+                }
+                if (e.which === 13) { //enter
+                    selected = filterOptions[curPosition]
+                        // setTrSelectedStyle(selected)
+                    $("#table-option").hide()
+                    show = false
+                    $("#txtGender").val(selected.text)
+                }
+                if (e.which === 9) { //Tab
+                    selected = filterOptions[curPosition]
+                        // setTrSelectedStyle(selected)
+                    $("#table-option").hide()
+                    show = false
+                }
+
+            }
+        });
+    });
+
     jQuery.fn.extend({
         getData: function() {
-        var tb = $("#table-option").attr("id");
-        if(tb == this.attr("id")) return a
+            var tb = $("#table-option").attr("id");
+            if (tb == this.attr("id")) return options
         },
         getValue: function() {
             var tb = $("#table-option").attr("id");
-            if(tb == this.attr("id")){
-                return a[selected].value
+            if (tb == this.attr("id")) {
+                return selected.value
             }
         },
-        getText : function(){
+        getText: function() {
             var tb = $("#table-option").attr("id");
-            if(tb == this.attr("id")){
-                return a[selected].text
+            if (tb == this.attr("id")) {
+                return selected.text
             }
         }
-      });
-    
+    });
+
+    $("#txtGender").blur(() => {
+        let check = false
+        let val = $("#txtGender").val()
+        options.forEach(i => {
+            if (i.text == val) check = true
+        })
+        if (!check) $("#txtGender").css("border-color", "red")
+        else $("#txtGender").css("border-color", "black")
+    })
 })
 
-function setTrBeforeSelectedStyle(beforeSelected,selected){
-   
-    $("#table-option > tbody > tr").each(function(index, tr) { 
-        if($(tr).attr("data-optionid") == beforeSelected){
+function setTrBeforeSelectedStyle(curPosition) {
+
+    $("#table-option > tbody > tr").each(function(index, tr) {
+        if ($(tr).attr("value") == curPosition) {
             $(tr).siblings().removeClass('tr-before-selected');
-            if(beforeSelected!=selected)
             $(tr).addClass('tr-before-selected')
         }
-     });
-}
-
-function setTrSelectedStyle(num){
-    $("#table-option > tbody > tr").each(function(index, tr) { 
-        if($(tr).attr("data-optionid") == num){
-            $(tr).siblings().removeClass('tr-before-selected');
-            $(tr).siblings().removeClass('tr-selected');
-            $(tr).removeClass('tr-before-selected')
-            $(tr).addClass('tr-selected')
-        }
-     });
+    });
 }
